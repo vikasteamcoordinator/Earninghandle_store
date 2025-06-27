3fa7f9101bcba4062c26b692ade7893a116a9c8d
Vikash Tiwari <teamcoordinator@earminghandle.com>
2025-06-27 09:29:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# UOMO E-commerce Website Using ReactJS
+
+![uomo-ecommerce-website-reactjs](https://socialify.git.ci/shakti177/uomo-ecommerce-website-reactjs/image?description=1&font=Inter&forks=1&issues=1&language=1&name=1&owner=1&pattern=Solid&pulls=1&stargazers=1&theme=Auto)
+
+## 📌 Live Demo
+https://uomo-ecommerce-website.netlify.app/
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/fec4a869-298c-4e9c-8df6-cb6f132f7bd8/deploy-status)](https://app.netlify.com/sites/uomo-ecommerce-website/deploys)
+
+## 😎 Features
+
+- Sleek and modern UI inspired by top e-commerce platforms
+- Fully responsive, providing a seamless shopping experience on any device
+- Effortless cart management, allowing users to add items with ease
+- Detailed product views for informed purchasing decisions
+- Real-time cart total updates for a transparent checkout process
+- Adjustable item quantities directly from the product page, streamlining the shopping experience without repetitive clicks
+- Interactive 3D t-shirt model, enhancing user engagement with an immersive shopping experience powered by Three.js 
+
+## 🚀 What technologies were used?
+
+- React.js (Create React App)
+- React Router DOM for seamless navigation
+- Material-UI for modern and responsive UI components
+- State management with Redux Toolkit
+- Three.js for immersive 3D models
+- Responsive web design principles for a mobile-friendly experience
+
+## 🖥️ How can you clone and tweak this project?
+
+From your command line, first clone this repo:
+
+```
+# Clone this repository
+$ https://github.com/shakti177/uomo-ecommerce-website-reactjs.git
+
+# Go into the repository
+$ cd uomo-ecommerce-website-reactjs
+
+# Remove current origin repository
+$ git remote remove origin
+
+```
+
+Then you can install the dependencies using NPM:
+
+```
+# Install dependencies
+$ npm install
+
+# Start development server
+$ npm start
+```
+👨‍💻 Happy coding!
+---
---
+++ src/Components/Authentication/LoginSign/LoginSignUp.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import "./LoginSignUp.css";
+import { Link } from "react-router-dom";
+
+const LoginSignUp = () => {
+  const [activeTab, setActiveTab] = useState("tabButton1");
+
+  const handleTab = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <>
+      <div className="loginSignUpSection">
+        <div className="loginSignUpContainer">
+          <div className="loginSignUpTabs">
+            <p
+              onClick={() => handleTab("tabButton1")}
+              className={activeTab === "tabButton1" ? "active" : ""}
+            >
+              Login
+            </p>
+            <p
+              onClick={() => handleTab("tabButton2")}
+              className={activeTab === "tabButton2" ? "active" : ""}
+            >
+              Register
+            </p>
+          </div>
+          <div className="loginSignUpTabsContent">
+            {/* tab1 */}
+
+            {activeTab === "tabButton1" && (
+              <div className="loginSignUpTabsContentLogin">
+                <form>
+                  <input type="email" placeholder="Email address *" required />
+                  <input type="password" placeholder="Password *" required />
+                  <div className="loginSignUpForgetPass">
+                    <label>
+                      <input type="checkbox" className="brandRadio" />
+                      <p>Remember me</p>
+                    </label>
+                    <p>
+                      <Link to="/resetPassword">Lost password?</Link>
+                    </p>
+                  </div>
+                  <button>Log In</button>
+                </form>
+                <div className="loginSignUpTabsContentLoginText">
+                  <p>
+                    No account yet?{" "}
+                    <span onClick={() => handleTab("tabButton2")}>
+                      Create Account
+                    </span>
+                  </p>
+                </div>
+              </div>
+            )}
+
+            {/* Tab2 */}
+
+            {activeTab === "tabButton2" && (
+              <div className="loginSignUpTabsContentRegister">
+                <form>
+                  <input type="text" placeholder="Username *" required />
+                  <input type="email" placeholder="Email address *" required />
+                  <input type="password" placeholder="Password *" required />
+                  <p>
+                    Your personal data will be used to support your experience
+                    throughout this website, to manage access to your account,
+                    and for other purposes described in our
+                    <Link
+                      to="/terms"
+                      style={{ textDecoration: "none", color: "#c32929" }}
+                    >
+                      {" "}
+                      privacy policy
+                    </Link>
+                    .
+                  </p>
+                  <button>Register</button>
+                </form>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginSignUp;
